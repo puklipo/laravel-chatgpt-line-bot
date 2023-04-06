675915e50e88c9cb2d5ce0a8d0b3439bf138ee82
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-04-06 13:56:10 +0900

    OpenAI

--- app/Listeners/Message/TextMessageListener.php
+++ app/Listeners/Message/TextMessageListener.php
@@ -1,10 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Listeners\Message;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Arr;
 use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use OpenAI\Laravel\Facades\OpenAI;
 use Revolution\Line\Facades\Bot;
 
 class TextMessageListener
@@ -27,7 +31,20 @@ class TextMessageListener
      */
     public function handle(TextMessage $event): void
     {
-        $response = Bot::replyText($event->getReplyToken(), $event->getText());
+        $token = $event->getReplyToken();
+        $text = $event->getText();
+
+        $chat = OpenAI::chat()->create([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => [
+                ['role' => 'user', 'content' => $text],
+            ],
+        ]);
+
+        $content = Arr::get($chat, 'choices.0.message.content');
+
+        $response = Bot::reply(token: $token)
+                       ->text($content);
 
         if (! $response->isSucceeded()) {
             logger()->error(static::class.$response->getHTTPStatus(), $response->getJSONDecodedBody());
