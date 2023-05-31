91c05ddce67d01cfe7452809d96b30474f8771e3
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-05-31 09:15:04 +0900

    Update SDK

---
+++ app/Listeners/Line/MessageListener.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Listeners\Line;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Arr;
+use LINE\Clients\MessagingApi\ApiException;
+use LINE\Webhook\Model\MessageEvent;
+use LINE\Webhook\Model\TextMessageContent;
+use OpenAI\Laravel\Facades\OpenAI;
+use Revolution\Line\Facades\Bot;
+
+class MessageListener
+{
+    protected string $token;
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param MessageEvent $event
+     * @return void
+     *
+     * @throws ApiException
+     */
+    public function handle(MessageEvent $event): void
+    {
+        $message = $event->getMessage();
+        $this->token = $event->getReplyToken();
+
+        match (get_class($message)) {
+            TextMessageContent::class => $this->text($message),
+        };
+    }
+
+    /**
+     * @throws ApiException
+     */
+    protected function text(TextMessageContent $message): void
+    {
+        $text = $message->getText();
+
+        $chat = OpenAI::chat()->create([
+            'model' => 'gpt-3.5-turbo',
+            'temperature' => 0.7,
+            'max_tokens' => 500,
+            'messages' => [
+                ['role' => 'user', 'content' => $text],
+            ],
+        ]);
+
+        $content = Arr::get($chat, 'choices.0.message.content');
+
+        if (blank($content)) {
+            return;
+        }
+
+        Bot::reply($this->token)->text($content);
+    }
+}
