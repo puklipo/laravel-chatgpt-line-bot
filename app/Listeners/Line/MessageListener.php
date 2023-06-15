e770d8c4d928debdea57f96241926e6592ac6750
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-06-15 11:04:37 +0900

    Update MessageListener.php

--- app/Listeners/Line/MessageListener.php
+++ app/Listeners/Line/MessageListener.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Listeners\Line;
@@ -29,7 +30,7 @@ class MessageListener
     /**
      * Handle the event.
      *
-     * @param MessageEvent $event
+     * @param  MessageEvent  $event
      * @return void
      *
      * @throws ApiException
